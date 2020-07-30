4d9647f0db714520fef3a5c119a01c3c852dbdb3
Nico Bollmann <nico.bollmann94@gmail.com>
2020-07-30 11:03:44 +0200

    Database connection

---
+++ backend/src/models/Patient.ts
@@ -0,0 +1,21 @@
+enum Gender {M, F, D};
+
+class Patient {
+   patientId: number;
+   surName: string;
+   firstName: string;
+   gender: Gender;
+   dateOfBirth: Date;
+   preExistingIllness: string;
+   miscellaneous: string;
+   AIsSelected: boolean;
+   AText: string;
+   BIsSelected: boolean;
+   BText: string;
+   CIsSelected: boolean;
+   CText: string;
+   DIsSelected: boolean;
+   DText: string;
+   EIsSelected: boolean;
+   EText: string;
+}
---
+++ backend/src/models/Pulsoxy.ts
@@ -0,0 +1,6 @@
+class Pulsoxy {
+   patientId: number;
+   timestamp: Date;
+   pulsrate: number;
+   spo2: number;
+}
