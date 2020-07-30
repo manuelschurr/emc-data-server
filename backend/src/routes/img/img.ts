b9e04ab3d965873579d80ab1d122907c824e2894
Manuel Schurr <admin@manuelschurr.com>
2020-07-30 15:26:37 +0200

    Added image upload function

---
+++ backend/src/routes/img/img.ts
@@ -0,0 +1,32 @@
+import express from "express"
+import multer from "multer"
+import { BadRequestResponse, SuccessResponse } from "../../core/ApiResponse"
+import asyncHandler from "../../helpers/asyncHandler"
+
+const router = express.Router()
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+// const upload = multer({dest:"../uploads"})
+
+router.post(
+    "/upload", upload.single("img"),
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    asyncHandler(async (req, res, next) => {
+        const file = req.file
+        if (!file) {
+            return new BadRequestResponse("No file provided").send(res)
+        }
+        return new SuccessResponse("Successful", file).send(res)
+    }),
+)
+
+export default router
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import getPatient from "./getPatient/getPatient"
 import getRtw from "./getRtw/getRtw"
+import img from "./img/img"
 import test from "./test/test"
 
 const router = express.Router()
@@ -8,5 +9,6 @@ const router = express.Router()
 router.use("/test", test)
 router.use("/getPatient", getPatient)
 router.use("/getRtw", getRtw)
+router.use("/img", img)
 
 export default router
