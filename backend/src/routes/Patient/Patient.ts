91005f06e7029a6caa101879e55f4a6c1fc57b65
Tim <17224650+TCfCoT@users.noreply.github.com>
2020-07-30 14:37:58 +0200

    Update Patient.ts

--- backend/src/routes/Patient/Patient.ts
+++ backend/src/routes/Patient/Patient.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { SuccessResponse } from "../../core/ApiResponse";
-import Database from "../../core/Database";
+import Database, { insertMultiplePulsoxyData } from "../../core/Database";
 import asyncHandler from "../../helpers/asyncHandler";
 
 const router = express.Router()
@@ -10,6 +10,14 @@ router.get(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     asyncHandler(async (req, res, next) => {
         return new SuccessResponse("Successful", Database.findLatestPulsoxyObject(1)).send(res)
+    }),
+)
+
+router.post(
+    "/postPulo",
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    asyncHandler(async (req, res, next) => {
+        insertMultiplePulsoxyData(res)
     }),
 )
 
