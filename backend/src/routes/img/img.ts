e15bd5aa1beb7da7203aa4b9a0e7db35516d7873
Manuel Schurr <admine@manuelschurr.com>
2020-08-05 10:45:47 +0200

    Added upload directory

--- backend/src/routes/img/img.ts
+++ backend/src/routes/img/img.ts
@@ -15,7 +15,6 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage })
 
-// const upload = multer({dest:"../uploads"})
 
 router.post(
     "/upload", upload.single("img"),
