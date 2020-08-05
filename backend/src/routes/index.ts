3d3f0bfce1f29dd52e989644599ad752ac3c19d4
Manuel Schurr <admine@manuelschurr.com>
2020-08-05 13:39:18 +0200

    properly removed test route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,9 @@ import express from "express"
 import img from "./img/img"
 import patient from "./patient/patient"
 import rtw from "./rtw/rtw"
-import test from "./test/test"
 
 const router = express.Router()
 
-router.use("/test", test)
 router.use("/patient", patient)
 router.use("/rtw", rtw)
 router.use("/img", img)
