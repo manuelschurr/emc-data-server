3b4209d403a6e89be595a463ee1bc0a548d88cd4
KieferJan <jkiefer95@gmail.com>
2020-08-19 14:27:31 +0200

    improve multi selection

--- frontend/trauma-room/src/components/pulsOxy.vue
+++ frontend/trauma-room/src/components/pulsOxy.vue
@@ -1,56 +1,16 @@
 <template>
   <div>
-    <div class="row align-items-start">
+    <div class="row align-items-start" v-if="selectedElements < 2">
       <div class="col-12">
         <form class="form-inline" style="align-items: center;">
-          <div v-if="selectedElements < 2" class="col-9">
+          <div class="col-9">
             <puls-oxy-line
               class="small"
               v-if="loaded"
               :chart-data="pulseChartData"
             />
           </div>
-          <div v-if="selectedElements < 2" class="col-3 pulseColor">
-            <br />
-            <b>Pulse</b>
-            <br />
-            <span
-              v-if="(loaded && lastPulse > 130) || lastPulse < 60"
-              class="bigFont notOkPulseOxy"
-              >{{ lastPulse }}</span
-            >
-            <span v-else class="bigFont">{{ lastPulse }}</span>
-            <svg
-              v-if="(loaded && lastPulse > 130) || lastPulse < 60"
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              class="bi bi-heart notOkPulseOxy"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
-              />
-            </svg>
-            <svg
-              v-else
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              class="bi bi-heart"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
-              />
-            </svg>
-            <br />PR/min
-          </div>
-          <div v-if="selectedElements >= 2" class="col-3 pulseColor">
+          <div class="col-3 pulseColor">
             <br />
             <b>Pulse</b>
             <br />
