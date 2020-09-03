a21e31eadcfef396318635ff207ba53c87f2ac21
marcguldner <61876401+marcguldner@users.noreply.github.com>
2020-09-03 14:58:05 +0200

    PulseOxy color change

--- frontend/trauma-room/src/components/pulseOxy.vue
+++ frontend/trauma-room/src/components/pulseOxy.vue
@@ -6,24 +6,19 @@
           <div class="col-9">
             <puls-oxy-line class="small" v-if="loaded" :chart-data="pulseChartData" />
           </div>
-          <div class="col-3 pulseColor">
+          <div v-if="lastPulse > 130 || lastPulse < 50" class="col-3 notOkPulseOxyColor">
             <br />
             <b>Pulse</b>
             <br />
-            <div v-if="loaded && (lastPulse > 130 || lastPulse < 50)">
-              <div v-if="!pulseSoundPlayed">{{ this.playSound()}} {{ this.pulseSoundPlayed = true }}</div>
-              <span class="bigFont notOkPulseOxy">{{ lastPulse }}</span>
-            </div>
-            <div v-else>
-              {{ this.pulseSoundPlayed = false }}
+            <div v-if="loaded">
+              <div v-show="false" v-if="!pulseSoundPlayed">{{ this.playSound()}} {{ this.pulseSoundPlayed = true }}</div>
               <span class="bigFont">{{ lastPulse }}</span>
             </div>
             <svg
-              v-if="loaded && (lastPulse > 130 || lastPulse < 50)"
               width="1em"
               height="1em"
               viewBox="0 0 16 16"
-              class="bi bi-heart notOkPulseOxy"
+              class="bi bi-heart"
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -32,8 +27,17 @@
                 d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
               />
             </svg>
+            <br />PR/min
+          </div>
+          <div v-else class="col-3 pulseColor">
+            <br />
+            <b>Pulse</b>
+            <br />
+            <div v-if="loaded">
+              {{ this.pulseSoundPlayed = false }}
+              <span class="bigFont">{{ lastPulse }}</span>
+            </div>
             <svg
-              v-else
               width="1em"
               height="1em"
               viewBox="0 0 16 16"
@@ -55,17 +59,25 @@
           <div class="col-9">
             <puls-oxy-line class="small" v-if="loaded" :chart-data="spo2ChartData" />
           </div>
-          <div class="col-3 spo2Color">
+          <div v-if="lastSpo2 < 90" class="col-3 notOkPulseOxyColor">
             <br />
             <b>
               SpO<sub>2</sub>
             </b>
             <br />
-            <div v-if="loaded && (lastSpo2 >= 100 || lastSpo2 < 90)">
-              <div v-if="!spo2SoundPlayed">{{ this.playSound()}} {{ this.spo2SoundPlayed = true }}</div>
-              <span class="bigFont notOkPulseOxy">{{ lastSpo2 }}</span>
-            </div>
-            <div v-else>
+            <div v-if="loaded">
+              <div v-show="false" v-if="!spo2SoundPlayed">{{ this.playSound()}} {{ this.spo2SoundPlayed = true }}</div>
+              <span class="bigFont">{{ lastSpo2 }}</span>
+            </div>O<sub>2</sub>
+            <br />SpO<sub>2</sub>%
+          </div>
+          <div v-else class="col-3 spo2Color">
+            <br />
+            <b>
+              SpO<sub>2</sub>
+            </b>
+            <br />
+            <div v-if="loaded">
               {{ this.spo2SoundPlayed = false }}
               <span class="bigFont">{{ lastSpo2 }}</span>
             </div>O<sub>2</sub>
@@ -75,34 +87,15 @@
       </div>
     </div>
     <div class="row" v-else>
-      <div class="col pulseColor">
+      <div v-if="lastPulse > 130 || lastPulse < 50" class="col notOkPulseOxyColor">
         <br />
         <b>Pulse</b>
         <br />
-        <div v-if="loaded && (lastPulse > 130 || lastPulse < 50)">
-          <div v-if="!pulseSoundPlayed">{{ this.playSound()}} {{ this.pulseSoundPlayed = true }}</div>
-          <span class="bigFont notOkPulseOxy">{{ lastPulse }}</span>
-        </div>
-        <div v-else>
-          {{ this.pulseSoundPlayed = false }}
+        <div v-if="loaded">
+          <div v-show="false" v-if="!pulseSoundPlayed">{{ this.playSound()}} {{ this.pulseSoundPlayed = true }}</div>
           <span class="bigFont">{{ lastPulse }}</span>
         </div>
         <svg
-          v-if="loaded && (lastPulse > 130 || lastPulse < 50)"
-          width="1em"
-          height="1em"
-          viewBox="0 0 16 16"
-          class="bi bi-heart notOkPulseOxy"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
-          />
-        </svg>
-        <svg
-          v-else
           width="1em"
           height="1em"
           viewBox="0 0 16 16"
@@ -117,17 +110,48 @@
         </svg>
         <br />PR/min
       </div>
-      <div class="col spo2Color">
+      <div v-else class="col pulseColor">
+        <br />
+        <b>Pulse</b>
+        <br />
+        <div v-if="loaded">
+          {{ this.pulseSoundPlayed = false }}
+          <span class="bigFont">{{ lastPulse }}</span>
+        </div>
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-heart"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"
+          />
+        </svg>
+        <br />PR/min
+      </div>
+      <div v-if="lastSpo2 < 90" class="col notOkPulseOxyColor">
         <br />
         <b>
           SpO<sub>2</sub>
         </b>
         <br />
-        <div v-if="loaded && (lastSpo2 >= 100 || lastSpo2 < 90)">
-          <div v-if="!spo2SoundPlayed">{{ this.playSound()}} {{ this.spo2SoundPlayed = true }}</div>
-          <span class="bigFont notOkPulseOxy">{{ lastSpo2 }}</span>
-        </div>
-        <div v-else>
+        <div v-if="loaded">
+          <div v-show="false" v-if="!spo2SoundPlayed">{{ this.playSound()}} {{ this.spo2SoundPlayed = true }}</div>
+          <span class="bigFont">{{ lastSpo2 }}</span>
+        </div>O<sub>2</sub>
+        <br />SpO<sub>2</sub>%
+      </div>
+      <div v-else class="col spo2Color">
+        <br />
+        <b>
+          SpO<sub>2</sub>
+        </b>
+        <br />
+        <div v-if="loaded">
           {{ this.spo2SoundPlayed = false }}
           <span class="bigFont">{{ lastSpo2 }}</span>
         </div>O<sub>2</sub>
@@ -412,12 +436,11 @@ export default {
 </script>
 
 <style>
-.notOkPulseOxy {
-  color: red;
-}
-.small {
-  max-height: 220px;
+.notOkPulseOxyColor {
   margin: 20px 0 20px;
+  align-self: stretch;
+  background-color: red;
+  color: white;
 }
 .pulseColor {
   margin: 20px 0 20px;
@@ -430,6 +453,10 @@ export default {
   align-self: stretch;
   background-color: #36c1e7;
   color: white;
+}
+.small {
+  max-height: 220px;
+  margin: 20px 0 20px;
 }
 .bigFont {
   font-size: 64px;
