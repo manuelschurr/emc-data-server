95420422df0a4c697a56b1ff2ea1052c54093b1c
KieferJan <jkiefer95@gmail.com>
2020-09-07 14:10:31 +0200

    create routes for api token

---
+++ backend/src/database/model/ApiKey.ts
@@ -0,0 +1,32 @@
+import { Document, model, Schema } from "mongoose"
+
+export const DOCUMENT_NAME = "ApiKey"
+export const COLLECTION_NAME = "apiKey"
+
+export default interface ApiKey extends Document {
+    apiKeyId: number
+    value: number
+    timestamp: number
+}
+
+const schema = new Schema(
+    {
+        apiKeyId: {
+            type: Schema.Types.String,
+            required: true,
+        },
+        value: {
+            type: Schema.Types.String,
+            required: true,
+        },
+        timestamp: {
+            type: Schema.Types.Date,
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+    },
+)
+
+export const ApiKeyModel = model<ApiKey>(DOCUMENT_NAME, schema, COLLECTION_NAME)
