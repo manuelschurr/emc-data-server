9194bd306d9ac00ff476d895b674c56ee76dedb9
marcguldner <61876401+marcguldner@users.noreply.github.com>
2020-09-09 18:29:55 +0200

    move files

---
+++ frontend/src/components/leftSidebar.vue
@@ -0,0 +1,195 @@
+<template>
+  <div>
+    <div class="btn-group-vertical" data-toggel="buttons" role="group">
+      <button
+        @click="setSelection($event)"
+        id="multi-selection"
+        class="btn btn-secondary"
+        :disabled="showComponentMultiSelection"
+        :class="toggleButton(showComponentMultiSelection)"
+      >
+        Gesamtübersicht
+      </button>
+      <button
+        @click="setSelection($event)"
+        id="btn-puls"
+        class="btn btn-secondary"
+        :disabled="showComponentPulseoxy"
+        :class="toggleButton(showComponentPulseoxy)"
+      >
+        Puls Oxy
+        <br />
+        <div v-if="lastPulse != null && (lastPulse < 50 || lastPulse > 120)" class="notOkPulseOxy">
+          Puls: {{ lastPulse }}
+        </div>
+        <div v-else-if="lastPulse == null" class="okPulseOxy">Keine Puls Daten verfügbar</div>
+        <div v-else class="okPulseOxy">Puls: {{ lastPulse }}</div>
+        <div v-if="lastSpo2 != null && lastSpo2 < 90" class="notOkPulseOxy">
+          SpO2: {{ lastSpo2 }}
+        </div>
+        <div v-else-if="lastSpo2 == null" class="okPulseOxy">Keine SpO2 Daten verfügbar</div>
+        <div v-else class="okPulseOxy">SpO2: {{ lastSpo2 }}</div>
+      </button>
+      <button
+        @click="setSelection($event)"
+        id="btn-stream"
+        class="btn btn-secondary"
+        :disabled="showComponentStream"
+        :class="toggleButton(showComponentStream)"
+      >
+        Bilder RTW
+      </button>
+      <button
+        @click="setSelection($event)"
+        id="btn-position"
+        class="btn btn-secondary"
+        :disabled="showComponentMap"
+        :class="toggleButton(showComponentMap)"
+      >
+        Position
+        <br />
+        ETA: {{ arrivalTime ? arrivalTime : "Fehler bei Routen Schnittstelle" }}
+      </button>
+      <PulseOxy v-if="false" />
+      <Stream v-if="false" />
+      <Maps v-if="false" />
+    </div>
+  </div>
+</template>
+
+<script>
+import PulseOxy from "./pulseOxy.vue";
+import Stream from "./stream.vue";
+import Maps from "./maps.vue";
+export default {
+  data() {
+    return {
+      showComponentMap: false,
+      showComponentStream: false,
+      showComponentPulseoxy: false,
+      showComponentMultiSelection: false,
+      pastEvent: null,
+      selection: [],
+      lastPulse: null,
+      lastSpo2: null
+    };
+  },
+  components: {
+    PulseOxy,
+    Stream,
+    Maps
+  },
+  mounted() {
+    this.$root.$on("selectedComponent", data => {
+      if (data === "btn-puls") {
+        this.selection = [];
+        this.selection.push(PulseOxy);
+      } else if (data === "btn-stream") {
+        this.selection = [];
+        this.selection.push(Stream);
+      } else if (data === "btn-position") {
+        this.selection = [];
+        this.selection.push(Maps);
+      } else if (data === "multi-selection") {
+        this.selection = [];
+        this.selection.push(Maps);
+        this.selection.push(Stream);
+        this.selection.push(PulseOxy);
+      }
+    });
+
+    this.$root.$on("lastPulseData", data => {
+      this.lastPulse = data;
+    });
+    this.$root.$on("lastSpo2Data", data => {
+      this.lastSpo2 = data;
+    });
+    this.$watch(
+      "arrivalTime",
+      arrivalTime => {
+        this.arrivalTime = arrivalTime;
+      },
+      { immediate: true }
+    );
+    this.setSelection({
+      currentTarget: {
+        id: "multi-selection"
+      }
+    });
+  },
+  props: {
+    arrivalTime: String
+  },
+  methods: {
+    setSelection(event) {
+      //var component = null;
+
+      if (event.currentTarget.id === "btn-puls") {
+        this.showComponentPulseoxy = !this.showComponentPulseoxy;
+        this.showComponentMap = false;
+        this.showComponentStream = false;
+        this.showComponentMultiSelection = false;
+        this.selection = [];
+        this.selection.push(PulseOxy);
+        //component = PulseOxy;
+      } else if (event.currentTarget.id === "btn-stream") {
+        this.showComponentStream = !this.showComponentStream;
+        this.showComponentPulseoxy = false;
+        this.showComponentMap = false;
+        this.showComponentMultiSelection = false;
+        this.selection = [];
+        this.selection.push(Stream);
+      } else if (event.currentTarget.id === "btn-position") {
+        this.showComponentMap = !this.showComponentMap;
+        this.showComponentPulseoxy = false;
+        this.showComponentStream = false;
+        this.showComponentMultiSelection = false;
+        this.selection = [];
+        this.selection.push(Maps);
+      } else if (event.currentTarget.id === "multi-selection") {
+        this.showComponentMultiSelection = !this.showComponentMultiSelection;
+        this.showComponentPulseoxy = false;
+        this.showComponentStream = false;
+        this.showComponentMap = false;
+        this.selection = [];
+        this.selection.push(Maps);
+        this.selection.push(Stream);
+        this.selection.push(PulseOxy);
+      }
+
+      this.$root.$emit("selectedComponent", this.selection);
+    },
+    pushComponentToList(comp) {
+      if (!this.selection.includes(comp)) {
+        this.selection.push(comp);
+      } else {
+        let index = this.selection.indexOf(comp);
+        if (index > -1) {
+          this.selection.splice(index, 1);
+        }
+      }
+    },
+    toggleButton(status) {
+      let buttonClass = "";
+      if (status) {
+        buttonClass = "okABCDE";
+      }
+      return buttonClass;
+    }
+  }
+};
+</script>
+<style scoped>
+.okABCDE {
+  background-color: #5cb85c;
+}
+.notOkABCDE {
+  background-color: red;
+}
+.okPulseOxy {
+  color: white;
+}
+.notOkPulseOxy {
+  color: red;
+}
+</style>
---
+++ frontend/src/components/stream.vue
@@ -0,0 +1,121 @@
+<template>
+  <div v-if="selectedElements <= 2">
+    <div
+      class="gallery"
+      id="mainDiv"
+      v-if="this.loading && this.captures.length != 0"
+    >
+      <div class="gallery-panel">
+        <img v-bind:src="chosenImage" width="100%" />
+      </div>
+      <div class="row">
+        <div
+          class="itemSpacing"
+          v-for="screenshot in captures"
+          :key="screenshot"
+        >
+          <img
+            v-on:click="clickMethod($event)"
+            v-bind:src="screenshot"
+            height="87"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-else>Aktuell wurden noch keine Bilder gemacht</div>
+  </div>
+  <div v-else>
+    <div
+      class="gallery"
+      id="mainDiv"
+      v-if="this.loading && this.captures.length != 0"
+    >
+      <div class="gallery-panel">
+        <img v-bind:src="chosenImage" width="70%" />
+      </div>
+    </div>
+    <div v-else>Aktuell wurden noch keine Bilder gemacht</div>
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  name: "camera",
+  data() {
+    return {
+      captures: [],
+      imageStrings: [],
+      chosenImage: null,
+      loading: false,
+    };
+  },
+  props: {
+    selectedElements: Number,
+  },
+  mounted() {
+    this.fillData();
+  },
+  created() {
+    this.timer = setInterval(this.fillData, 5000);
+  },
+  methods: {
+    clickMethod: function (event) {
+      this.chosenImage = event.currentTarget.src;
+    },
+    async fillData() {
+      var vm = this;
+      this.loading = true;
+
+      var body = "";
+      var config = {
+        method: "get",
+        //"https://localhost:3000/img/all",
+        url: "https://134.155.48.211:3000/img/all",
+        headers: {},
+        data: body,
+      };
+
+      axios(config).then(function (response) {
+        if (response.data.statusCode === "10000") {
+          var newImageStrings = response.data.data;
+          var newToAdd = [];
+          for (var image of newImageStrings) {
+            if (!vm.imageStrings.includes(image)) {
+              vm.imageStrings.push(image);
+              newToAdd.push(image);
+            }
+          }
+          for (var imageStr of newToAdd) {
+            var bodyTwo = "";
+            var configGetImages = {
+              method: "get",
+              //"https://localhost:3000/img/single/" + imageStr,
+              url: "https://134.155.48.211:3000/img/single/" + imageStr,
+              responseType: "blob",
+              headers: {},
+              data: bodyTwo,
+            };
+            axios(configGetImages).then(function (responseImages) {
+              if (response.data.statusCode === "10000") {
+                var pic = URL.createObjectURL(responseImages.data);
+                vm.captures.push(pic);
+                vm.chosenImage = vm.captures[vm.captures.length - 1];
+              }
+            });
+          }
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.itemSpacing {
+  margin: 5px 2px 0px 2px;
+}
+.row {
+  margin: 0px 0px;
+}
+</style>
