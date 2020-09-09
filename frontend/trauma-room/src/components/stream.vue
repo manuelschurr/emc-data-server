1c2c7036f8e9b4347005dd3482ed2973e52759cd
marcguldner <61876401+marcguldner@users.noreply.github.com>
2020-09-09 16:09:11 +0200

    Pics are appearing now (localhost)

--- frontend/trauma-room/src/components/stream.vue
+++ frontend/trauma-room/src/components/stream.vue
@@ -1,222 +1,82 @@
 <template>
-  <div>
-    <div>
-      <div class="container">
-        <div
-          class="carousel slide"
-          id="main-carousel"
-          data-interval="false"
-          data-ride="carousel"
-        >
-          <ol class="carousel-indicators">
-            <li
-              data-target="#main-carousel"
-              data-slide-to="0"
-              class="active"
-            ></li>
-
-            <li data-target="#main-carousel" data-slide-to="1"></li>
-            <li data-target="#main-carousel" data-slide-to="2"></li>
-            <li data-target="#main-carousel" data-slide-to="3"></li>
-          </ol>
-          <!-- /.carousel-indicators -->
-
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img
-                class="d-block img-fluid"
-                src="../assets/ambulance.png"
-                alt
-              />
-              <div class="carousel-caption d-none d-md-block"></div>
-            </div>
-            <div class="carousel-item">
-              <img
-                class="d-block img-fluid"
-                src="../assets/project_logo.png"
-                alt
-              />
-              <div class="carousel-caption d-none d-md-block"></div>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="../assets/umm_logo.png" alt />
-              <div class="carousel-caption d-none d-md-block"></div>
-            </div>
-            <div class="carousel-item">
-              <img
-                src="../assets/project_logo.png"
-                alt
-                class="d-block img-fluid"
-              />
-              <div class="carousel-caption d-none d-md-block"></div>
-            </div>
-          </div>
-          <!-- /.carousel-inner -->
-          <a
-            href="#main-carousel"
-            class="carousel-control-prev"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon"></span>
-            <span class="sr-only" aria-hidden="true">Prev</span>
-          </a>
-          <a
-            href="#main-carousel"
-            class="carousel-control-next"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon"></span>
-            <span class="sr-only" aria-hidden="true">Next</span>
-          </a>
-        </div>
+  <div class="gallery" id="mainDiv" v-if="this.loading">
+    <div class="gallery">
+      <div class="gallery-panel">
+        <img v-bind:src="chosenImage" width="100%" />
       </div>
-      <!-- <video ref="video" id="video" width="1024" height="576" autoplay></video> -->
-      <!-- <div>
-      <button
-        class="btn btn-dark btn-lg"
-        style="margin-top: 5px; margin-bottom: 7px"
-        id="snap"
-        v-on:click="capture()"
-      >
-        <i class="fa fa-camera"></i>
-      </button>
-    </div>-->
-      <!-- <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
-    <ul>
-      <li v-for="c in captures" :key="c.id">
-        <img v-bind:src="c" height="50" />
-      </li>
-    </ul>-->
     </div>
+    <div class="row">
+      <div class="itemSpacing" v-for="screenshot in captures" :key="screenshot">
+        <img v-on:click="clickMethod($event)" v-bind:src="screenshot" height="87" />
+      </div>
+    </div>
+    <!-- <div class="canvas" id="galleryCanvas"></div> -->
   </div>
 </template>
 
 <script>
+const axios = require("axios");
 export default {
   name: "camera",
   data() {
     return {
-      captures: [
-        {
-          src: "../assets/umm_logo.png",
-          index: 1,
-        },
-        {
-          src: "../assets/ambulance.png",
-          index: 2,
-        },
-        {
-          src: "../assets/project_logo.png",
-          index: 3,
-        },
-        {
-          src: "../assets/umm_team_logo.png",
-          index: 4,
-        },
-      ],
+      captures: [],
+      chosenImage: null,
+      loading: false,
     };
   },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    clickMethod: function (event) {
+      this.chosenImage = event.currentTarget.src;
+    },
+    async fillData() {
+      var vm = this;
+      this.loading = true;
+      // The first server request is to find the patient by the given patient ID
+      var body = "";
+      var config = {
+        method: "get",
+        url: "https://localhost:3000/img/all",
+        // "https://134.155.48.211:3000/img/all",
+        headers: {},
+        data: body,
+      };
+
+      axios(config).then(function (response) {
+        if (response.data.statusCode === "10000") {
+          console.log("Images " + response.data.data);
+          var imageStrings = response.data.data;
+          for (var image of imageStrings) {
+            var bodyTwo = "";
+            console.log("Images " + JSON.stringify(image));
+            var configGetImages = {
+              method: "get",
+              url: "https://localhost:3000/img/single/" + image,
+              // "https://134.155.48.211:3000/img/single/" + image,
+              responseType: "blob",
+              headers: {},
+              data: bodyTwo,
+            };
+            axios(configGetImages).then(function (responseImages) {
+              if (response.data.statusCode === "10000") {
+                var pic = URL.createObjectURL(responseImages.data);
+                vm.captures.push(pic);
+                vm.chosenImage = vm.captures[0];
+              }
+            });
+          }
+        }
+      });
+    },
+  },
 };
-//   props: {
-//     selectedElements: Number,
-//   },
-//   mounted() {
-//     this.initVideo();
-//   },
-//   methods: {
-//     initVideo() {
-//       this.video = this.$refs.video;
-//       if (
-//         "mediaDevices" in navigator &&
-//         "getUserMedia" in navigator.mediaDevices
-//       ) {
-//         // set ratio to 16:9
-//         // if (this.selectedElements == 3) {
-//         //   let constraints = {
-//         //     video: {
-//         //       width: {
-//         //         min: 640,
-//         //         ideal: 1280,
-//         //         max: 1920,
-//         //       },
-//         //       height: {
-//         //         min: 360,
-//         //         ideal: 720,
-//         //         max: 1080,
-//         //       },
-//         //     },
-//         //   };
-//         // } else {
-//         let constraints = {
-//           video: {
-//             width: {
-//               min: 640,
-//               ideal: 1280,
-//               max: 1920,
-//             },
-//             height: {
-//               min: 360,
-//               ideal: 720,
-//               max: 1080,
-//             },
-//           },
-//         };
-//         // }
-//         navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-//           const videoPlayer = document.querySelector("video");
-//           videoPlayer.srcObject = stream;
-//           videoPlayer.play();
-//         });
-//       } else {
-//         alert("Kein Livestream verfügbar aktuell...");
-//       }
-//     },
-//     capture() {
-//       // this.canvas = this.$refs.canvas;
-//       //this.canvas = document.querySelector("canvas");
-//       this.canvas = document.getElementById("galleryCanvas");
-//       var context = this.canvas.getContext("2d");
-//       context.drawImage(this.video, 0, 0, 640, 480);
-//       this.captures.push(this.canvas.toDataURL("image/png"));
-//       this.$root.$emit("selectScreenshots", this.captures);
-//     },
-//   },
-// };
 </script>
 
 <style scoped>
-/* #app {
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: aqua;
-} */
-/* #video {
-  background-color: #000000;
-} */
-/* #canvas {
-  display: none;
-} */
-/* li {
-  display: inline;
-  padding: 5px;
-} */
-/* .darkColor {
-  color: #000000;
-} */
-.carousel-inner img {
-  margin: auto;
-}
-.carousel-control-next {
-  background-color: rgba(160, 160, 160, 0.24);
-}
-.carousel-control-prev {
-  background-color: rgba(160, 160, 160, 0.24);
-}
-.carousel-control-prev-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E") !important;
-}
-.carousel-control-next-icon {
-  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E") !important;
+.itemSpacing {
+  margin: 5px 2px 0px 2px;
 }
 </style>
